47273d1c025f1bbc30b506c183f38977236b6b4d
roqisb <roqisb9@gmail.com>
2023-10-05 20:32:10 +0700

    done

---
+++ app/src/main/java/com/example/testing/layoutlinear.kt
@@ -0,0 +1,11 @@
+package com.example.testing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layoutlinear : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layoutlinear)
+    }
+}
